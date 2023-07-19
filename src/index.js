9a39d2b5751e1211ae73245c4c0ab13282e0c4e8
Victor <wanjala94vic@gmail.com>
2023-07-19 22:13:08 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", function() {
+  const inputBox = document.getElementById('new-task-description');
+  const listContainer = document.getElementById('tasks');
+
+  function addTask() {
+    if (inputBox.value === '') {
+      alert('You must write something!');
+    } else {
+      let li = document.createElement('li');
+      li.innerHTML = inputBox.value;
+      listContainer.appendChild(li);
+      let span = document.createElement('span');
+      span.innerHTML = '\u00d7';
+      li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+  }
+
+  listContainer.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('checked');
+      saveData();
+    } else if (e.target.tagName === 'SPAN') {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  }, false);
+
+  function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML);
+  }
+
+  function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data');
+  }
+
+  showTask();
+
+  const addTaskForm = document.getElementById('create-task-form');
+  addTaskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    addTask();
+  });
 });
